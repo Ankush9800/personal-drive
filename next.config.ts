c0b4c22837845eeb92718052cdfffa4c3d553ed1
Ankush9800 <149710234+Ankush9800@users.noreply.github.com>
2025-06-01 19:39:09 +0530

    Remove hardcoded domain from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'standalone',
   images: {
-    unoptimized: true, // Required for Cloudflare Pages
+    unoptimized: true // Required for Cloudflare Pages
   },
   experimental: {
     serverActions: {
       allowedOrigins: ["*"],
       bodySizeLimit: '2mb'
     }
+  },
+  // Required for Cloudflare Pages
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
   },
 };
 
